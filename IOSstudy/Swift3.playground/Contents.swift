dc2ff72a9eed1df6401e32278ebc207ccdbdc192
konghao <konghao8013@163.com>
2015-12-19 13:14:35 +0800

    增加数组学校4

--- IOSstudy/Swift3.playground/Contents.swift
+++ IOSstudy/Swift3.playground/Contents.swift
@@ -7,6 +7,16 @@ var str = "Hello, playground"
 
 str.hasPrefix("ho")
 
-var str1="Welcome to Play Swift ! Step by Step Learn Swift Lang"
-str.rangeOfString("Step")
+var str1="Welcome Step to Play Swift ! Step by Step Learn Step Swift Lang"
+str1.rangeOfString("Step",options: NSStringCompareOptions.BackwardsSearch)
+let searchRange=Range<String.Index>(start:str1.startIndex.advancedBy(22),end:str1.endIndex)
 
+str1.rangeOfString("Step",options:NSStringCompareOptions.CaseInsensitiveSearch,range:searchRange)
+
+str1.substringFromIndex(str1.startIndex.advancedBy(1))
+
+str1.substringToIndex(str1.startIndex.advancedBy(2))
+
+str1.substringWithRange(searchRange)
+
+str1.insert("z", atIndex:str1.startIndex.advancedBy(8))
